22cbf10efd0fb62476fcd465f2885d57e23372d6
Rashitko <michal.raska@gmail.com>
2015-03-25 18:47:32 +0100

    Vytvorene triedy Parser a OptionsDefinition

---
+++ src/options/OptionsDefinition.java
@@ -0,0 +1,17 @@
+package options;
+
+import java.util.List;
+
+public class OptionsDefinition {
+
+    private List<Option> options;
+
+    public boolean addOption(Option option) {
+        return false;
+    }
+
+    public Option deleteOption(Option option) {
+        return null;
+    }
+
+}
---
+++ src/options/Parser.java
@@ -0,0 +1,12 @@
+package options;
+
+public class Parser {
+
+    private OptionsDefinition optionsDefinition;
+    private String[] arguments;
+
+    public MagicClass fillOptions() {
+        return false;
+    }
+
+}
