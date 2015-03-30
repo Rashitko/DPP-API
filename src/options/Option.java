3a930e05f38594d4a64cfc60128a7e37bd88c4c3
Jakub Hava <jakub.hava@gmail.com>
2015-03-30 21:22:22 +0200

    Pulka Option okomentovana, zmeny parametry u hasShortSwitch na (String shortSwitch) z (Option option) a odpovidajicne u hasLongSwitch

--- src/options/Option.java
+++ src/options/Option.java
@@ -3,6 +3,9 @@ package options;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * This class represent the option
+ */
 public class Option {
 
     private final Set<String> shortSwitches;
@@ -13,6 +16,11 @@ public class Option {
     private final String description;
     private ParseError parseError;
 
+    /**
+     * This private constructor sets all the properties according to the Builder object on the input
+     *
+     * @param builder builder from which the properties are set
+     */
     private Option(Builder builder) {
         this.shortSwitches = builder.shortSwitches;
         this.longSwitches = builder.longSwitches;
@@ -22,41 +30,81 @@ public class Option {
         this.description = builder.description;
     }
 
-    public boolean hasShortSwitch(Option option) {
+    /**
+     * Checks whether this option has one particular short switch alternative
+     *
+     * @param shortSwitch short switch
+     * @return true if the option contains given short switch, otherwise false
+     */
+    public boolean hasShortSwitch(String shortSwitch) {
         //TODO: Check if shortSwitches contains option
         return false;
     }
 
+    /**
+     * Gets the set of all short switches
+     * @return the set of all short switches
+     */
     public Set<String> getShortSwitches() {
         return shortSwitches;
     }
 
-    public boolean hasLongSwitch(Option option) {
+    /**
+     * Checks whether this option has one particular long switch alternative
+     *
+     * @param longSwitch short switch
+     * @return true if the option contains given long switch, otherwise false
+     */
+    public boolean hasLongSwitch(String longSwitch) {
         //TODO: Check if longSwitches contains option
         return false;
     }
 
+    /**
+     * Gets the set of all long switches
+     * @return the set of all long switches
+     */
     public Set<String> getLongSwitches() {
         return longSwitches;
     }
 
+    /**
+     * Checks whether is this option mandatory or not
+     * @return true if this option is mandatory, otherwise false
+     */
     public boolean isRequired() {
         return required;
     }
 
+    /**
+     * Checks whether this option takes argument
+     * @return true if this option does take an argument, otherwise false
+     */
     public boolean hasArgument() {
         //TODO:
         return argument != null;
     }
 
+    /**
+     * Gets the argument associated with this option
+     * @return the argument associated with this option
+     */
     public Argument getArgument() {
         return argument;
     }
 
+    /**
+     * Checks if this option has mandatory, optional of forbidden argument
+     * @return the enum containing the information whether this option has mandatory, optional of forbidden argument
+     */
     public ArgumentPresence getArgumentPresence() {
         return argumentPresence;
     }
 
+    /**
+     * Gets the option description
+     * @return the option description
+     */
     public String getDescription() {
         return description;
     }
