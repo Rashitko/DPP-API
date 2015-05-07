fab598dd5ea0894492af49892bdef7b9b06861aa
kuba <jakub.hava@gmail.com>
2015-05-07 10:09:43 +0200

    pridana testNG knihovna do lib a zatim jen prazdny test z duvodu odladeni antu

---
+++ src/tests/ConstraintTest.java
@@ -0,0 +1,13 @@
+package tests;
+
+public class ConstraintTest {
+
+    @org.testng.annotations.Test
+    public void testIsFulfilled() throws Exception {
+    }
+
+    @org.testng.annotations.Test
+    public void testGetErrorMessage() throws Exception {
+
+    }
+}
