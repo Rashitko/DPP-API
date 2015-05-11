521c8947670c436bb18cde5060dc2d6014ca4c7f
kuba <jakub.hava@gmail.com>
2015-05-11 11:33:31 +0200

    Pridany testy pro kontrolu hranic pro IntegerArgumentParser

--- src/tests/IntegerArgumentParserTest.java
+++ src/tests/IntegerArgumentParserTest.java
@@ -17,16 +17,32 @@ public class IntegerArgumentParserTest {
     }
 
     @Test
-    public void test_PositiveNumber_OK() throws Exception {
+    public void test_PositiveNumber_Pass() throws Exception {
         Integer result = parser.parse("1");
         Integer expected = 1;
         Assert.assertEquals(result, expected);
     }
 
     @Test
-    public void test_NegativeNumber_OK() throws Exception {
+    public void test_PositiveNumberBoundary_Pass() throws Exception {
+        String integerMaxValue = Integer.MAX_VALUE + "";
+        Integer result = parser.parse(integerMaxValue);
+        Integer expected = Integer.MAX_VALUE;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void test_NegativeNumber_Pass() throws Exception {
         Integer result = parser.parse("-122");
         Integer expected = -122;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void test_NegativeNumberBoundary_Pass() throws Exception {
+        String integerMinValue = Integer.MIN_VALUE + "";
+        Integer result = parser.parse(integerMinValue);
+        Integer expected = Integer.MIN_VALUE;
         Assert.assertEquals(result, expected);
     }
 
