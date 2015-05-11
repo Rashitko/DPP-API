2c4d2a399aaedffc9d5766fa95148f242ea1e33b
kuba <jakub.hava@gmail.com>
2015-05-11 11:41:33 +0200

    Testy pro ShortParser ( stejny pattern jako pro IntegerParser) pridany

--- src/tests/ShortArgumentParserTest.java
+++ src/tests/ShortArgumentParserTest.java
@@ -1,64 +1,64 @@
 package tests;
 
-import DPPParser.parsers.IntegerArgumentParser;
 import DPPParser.parsers.ParsingException;
+import DPPParser.parsers.ShortArgumentParser;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 
-public class IntegerArgumentParserTest {
+public class ShortArgumentParserTest {
 
-    private IntegerArgumentParser parser;
+    private ShortArgumentParser parser;
 
     @BeforeClass
     public void initialize() {
-        parser = new IntegerArgumentParser();
+        parser = new ShortArgumentParser();
     }
 
     @Test
     public void testParse_PositiveNumber_Pass() throws Exception {
-        Integer result = parser.parse("1");
-        Integer expected = 1;
+        Short result = parser.parse("1");
+        Short expected = 1;
         Assert.assertEquals(result, expected);
     }
 
     @Test
     public void testParse_PositiveNumberBoundary_Pass() throws Exception {
-        String integerMaxValue = Integer.MAX_VALUE + "";
-        Integer result = parser.parse(integerMaxValue);
-        Integer expected = Integer.MAX_VALUE;
+        String shortMaxValue = Short.MAX_VALUE + "";
+        Short result = parser.parse(shortMaxValue);
+        Short expected = Short.MAX_VALUE;
         Assert.assertEquals(result, expected);
     }
 
     @Test
     public void testParse_NegativeNumber_Pass() throws Exception {
-        Integer result = parser.parse("-122");
-        Integer expected = -122;
+        Short result = parser.parse("-122");
+        Short expected = -122;
         Assert.assertEquals(result, expected);
     }
 
     @Test
     public void testParse_NegativeNumberBoundary_Pass() throws Exception {
-        String integerMinValue = Integer.MIN_VALUE + "";
-        Integer result = parser.parse(integerMinValue);
-        Integer expected = Integer.MIN_VALUE;
+        String shortMinValue = Short.MIN_VALUE + "";
+        Short result = parser.parse(shortMinValue);
+        Short expected = Short.MIN_VALUE;
         Assert.assertEquals(result, expected);
     }
 
     @Test(expectedExceptions = ParsingException.class)
     public void testParse_Letter_ThrowException() throws Exception {
-        Integer result = parser.parse("a");
+        Short result = parser.parse("a");
     }
 
     @Test(expectedExceptions = ParsingException.class)
     public void testParse_OutOfRange_ThrowException() throws Exception {
-        Integer result = parser.parse("999999999999999999999");
+        Short result = parser.parse("999999999999999999999");
     }
 
     @Test(expectedExceptions = ParsingException.class)
     public void testParse_FloatNumber_ThrowException() throws Exception {
-        Integer result = parser.parse("5.5");
+        Short result = parser.parse("5.5");
     }
 
     @Test
@@ -68,7 +68,7 @@ public class IntegerArgumentParserTest {
         } catch (ParsingException e) {
             // ignore the exception on purpose
         }
-        Assert.assertEquals(parser.getParseErrorMessage(), "a cannot be parsed as integer");
+        Assert.assertEquals(parser.getParseErrorMessage(), "a cannot be parsed as short");
     }
 
 }
