05937b480c360309dda89deb64bf8b58597a4056
kuba <jakub.hava@gmail.com>
2015-05-13 14:35:26 +0200

    rozpracovan test Parseru

--- src/tests/BasicParserTest.java
+++ src/tests/BasicParserTest.java
@@ -6,11 +6,15 @@ import DPPParser.options.Option;
 import DPPParser.options.OptionsList;
 import DPPParser.options.Parser;
 import DPPParser.parsers.IntegerArgumentParser;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 
-public class ParserRegularUseTest {
+
+public class BasicParserTest {
 
     final int SIZE_LOWER_BOUND = 0;
     final int SIZE_DEFAULT = 42;
@@ -39,40 +43,41 @@ public class ParserRegularUseTest {
 
         //INSTANTIATE PARSER
         parser = new Parser(options);
-    }
-
-    @Test
-    public void testResolveOptions() throws Exception {
-
+        String[] argArray = {"-v", "--version", "-s", "OLD", "--length=20", "--", "-my-file", "your-file"};
+        parser.resolveOptions(argArray);
     }
 
     @Test
     public void testGetFailedOptions() throws Exception {
-
+        Assert.assertEquals(parser.getFailedOptions().size(), 1);
     }
 
     @Test
     public void testGetExtraOptions() throws Exception {
-
+        Assert.assertEquals(parser.getExtraOptions().size(), 1);
     }
 
     @Test
     public void testGetMissedOptions() throws Exception {
-
+        Assert.assertEquals(parser.getMissedOptions().size(), 0);
     }
 
     @Test
     public void testHasError() throws Exception {
-
+        Assert.assertFalse(parser.hasError());
     }
 
     @Test
     public void testGetUnmatchedArguments() throws Exception {
-
+        Assert.assertEquals(parser.getUnmatchedArguments().size(), 0);
     }
 
     @Test
     public void testGetRegularArguments() throws Exception {
-
+        Assert.assertEquals(parser.getRegularArguments().size(), 2);
+        List<String> expected = new ArrayList<String>();
+        expected.add("-my-file");
+        expected.add("your-file");
+        Assert.assertEquals(parser.getRegularArguments(), expected);
     }
 }
