3c46272a7d882f2c8c91d07cdca1e267b2b97ec1
Michal Raška <michal.raska@gmail.com>
2015-05-13 12:26:36 +0200

    Implementovany example do Main

--- src/examples/Main.java
+++ src/examples/Main.java
@@ -1,33 +1,41 @@
 package examples;
 
 import DPPParser.arguments.Argument;
-import DPPParser.arguments.Constraint;
+import DPPParser.defaultConstrains.LowerBoundConstraint;
 import DPPParser.options.Option;
 import DPPParser.options.OptionsList;
 import DPPParser.options.Parser;
 import DPPParser.parsers.IntegerArgumentParser;
-import DPPParser.parsers.StringArgumentParser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
-    public static void main(final String[] args) {
-        System.out.printf("123 %b \n", isOption("123"));
-        System.out.printf("-123 %b \n", isOption("-123"));
-        System.out.printf("--123 %b \n", isOption("--123"));
-        System.out.printf("123.0 %b \n", isOption("123.0"));
-        System.out.printf("-123,0 %b \n", isOption("-123,0"));
-        System.out.printf("--123.0 %b \n", isOption("--123.0"));
-        System.out.printf("-123.0 %b \n", isOption("--123.0"));
-    }
+    public static final int SIZE_LOWER_BOUND = 0;
 
-    private static boolean isOption(String arg) {
-        try {
-            //noinspection ResultOfMethodCallIgnored
-            Double.parseDouble(arg);
-            return false;
-        } catch (NumberFormatException ignore) {
+    public static void main(final String[] args) {
+
+        Option verbose = new Option.Builder("v")
+                .addLongSwitch("verbose")
+                .build();
+        Option size = new Option.Builder("s")
+                .addLongSwitch("size")
+                .setOptionalArgument(new Argument<Integer>(new LowerBoundConstraint<Integer>(SIZE_LOWER_BOUND), new IntegerArgumentParser()))
+                .build();
+
+        OptionsList options = new OptionsList();
+        options.add(verbose);
+        options.add(size);
+        Parser parser = new Parser(options);
+        parser.resolveOptions(args);
+        System.out.println("verbose = " + verbose.isSuccessful());
+        System.out.println("size = " + size.getArgument().getValue());
+        List<String> unmatchedAndRegular = new ArrayList<String>(parser.getUnmatchedArguments());
+        unmatchedAndRegular.addAll(parser.getRegularArguments());
+        if (!unmatchedAndRegular.isEmpty()) {
+            System.out.println(unmatchedAndRegular);
         }
-        return arg.startsWith("-") && !arg.equals("--");
     }
 
 }
